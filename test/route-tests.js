4fb166a79a8bd3e8d8fd70ef5d2eeec681c23d91
amberwoodrow <amberwoodrow@gmail.com>
2015-11-09 16:17:40 -0700

    initial commit

---
+++ test/route-tests.js
@@ -0,0 +1,24 @@
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var mocha = require('mocha');
+var chaiHttp = require('chaiHttp');
+var server = require('../src/server/app.js');
+
+var should = chai.should();
+chai.use(chaiHttp);
+
+describe('server router', function() {
+  it('should return a json message', function(done) {
+    chai.request(server)
+    .get('/')
+    .end(function(err, res) {
+      console.log(res.text);
+      res.should.have(200);
+      res.title.should.be('Express');
+      // res.header.should.be('text/html');
+
+      done();
+    });
+  });
+});
